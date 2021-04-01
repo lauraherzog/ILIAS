df7563dc09b0386e0e9ec7a9aece01ac2d253b27
Laura Herzog <quellmalz.laura@gmail.com>
2021-04-01 08:26:47 +0200

    Tests

--- CI/PHP-CS-Fixer/run_check.sh
+++ CI/PHP-CS-Fixer/run_check.sh
@@ -7,9 +7,6 @@
 
 URL="https://api.github.com/repos/${GITHUB_REPOSITORY}/pulls/${PR_NUMBER}/files"
 CHANGED_FILES=$(curl -s -X GET -G $URL | jq -r '.[] | .filename' | grep '.php')
-echo $CHANGED_FILES
-
-exit
 
 for FILE in ${CHANGED_FILES}
 do
